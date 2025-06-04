f6a540a8fba065d7064a725e60960d9746a7a711
JVRicci <54457696+JVRicci@users.noreply.github.com>
2025-06-03 23:16:14 -0300

    doc/Adicao de comentarios explicativos

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -5,68 +5,75 @@ import { knex } from '../database'
 import { checkSessionIdExists } from '../middlewares/check-session-id-exists'
 
 export function transactionRoutes(app : FastifyInstance) {
+    // Retorna todas as transações
     app.get('/',
         {
             preHandler: [checkSessionIdExists]
         },
         async ( request, reply ) => {  
         // const tables = await knex('sqlite_schema').select('*')
-        const { sessionId } = request.cookies
+            const { sessionId } = request.cookies
 
-        const transaction = await knex('transactions')
-            .where('session_id', sessionId)
-            .select('*')
+            const transaction = await knex('transactions')
+                .where('session_id', sessionId)
+                .select('*')
 
-        return { transaction }
+            return { transaction }
     })
 
+    // Pesquisa pelo id
     app.get("/:id",
         {
             preHandler: [checkSessionIdExists]
         },
         async (request) =>{
-        const getTransactionParamsSchema = z.object({
-            id: z.string().uuid()
-        })
-
-        const { id } = getTransactionParamsSchema.parse(request.params)
-
-        const { sessionId } = request.cookies
-
-        const transaction = await knex('transactions')
-            .where({
-                session_id: sessionId,
-                id
+            const getTransactionParamsSchema = z.object({
+                id: z.string().uuid()
             })
-            .first()
 
-        return { transaction }
+            const { id } = getTransactionParamsSchema.parse(request.params)
+
+            const { sessionId } = request.cookies
+
+            const transaction = await knex('transactions')
+                .where({
+                    session_id: sessionId,
+                    id
+                })
+                .first()
+
+            return { transaction }
     })
 
+    // Faz soma de todos os valores das transações
     app.get('/summary',
         {
             preHandler: [checkSessionIdExists]
         },
         async (request) => {
 
-        const { session_id } = request.cookies
-        
-        // É possivel adcionar alias nas colunas, como é feito no metodo sum
-        const summary = await knex('transactions')
-            .sum('amount', { as : 'amount'})
-            .first()
-        
-        return { summary }
+            const { session_id } = request.cookies
+            
+            // É possivel adcionar alias nas colunas, como é feito no metodo sum
+            const summary = await knex('transactions')
+                .sum('amount', { as : 'amount'})
+                .first()
+            
+            return { summary }
     })
 
+    // Cria nova transação
     app.post('/',
         async (request, reply)=> {
+        // Cria um z object para tratamento das informaçoes do request, 
+        // e faz com que apenas dois tipos de transação sejam aceitas
         const createTransactionBodySchema = z.object({
             title: z.string(),
             amount: z.number(),
             type: z.enum(['credit', 'debit'])
         })
 
+        // Faz a verificação da requisição
         const {title, amount, type} = createTransactionBodySchema.parse(
             request.body
         )
